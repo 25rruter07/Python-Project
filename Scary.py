485452443eb5f4e39fdb9f20e7d6b446148a8c3f
26bzheng16 <70980620+26bzheng16@users.noreply.github.com>
2023-11-09 09:22:13 -0600

    Update Scary.py
    Updated sp allocation in both areas, added stuff to paths 1 and 2, added text to endings
    
    in next commit will add more graphics and finish wacky door

--- Scary.py
+++ Scary.py
@@ -116,29 +116,48 @@ class char:
       return self.name+' has '+str(self.attack) +' attack, '+str(self.charisma) +' charisma, '+str(self.defense)+' defense, and '+str(self.money)+' money.'
 
 finished = 1
+
+name = ""
+attack = 0
+charisma = 0
+defense = 0
+money = 0
+
 while finished == 1:
   name = input("Enter a name ")
   sp = 10
   while sp > 0:
+    choice1 = 0
+    choice2 = 0
+    choice3 = 0
+    choice4 = 0
     sceneset("black",'white', 300, '⚔')
-    attack = int(input("Enter attack value (Integer) "))
-    sp -= attack
+    choice1 = int(input("Enter attack value (Integer) "))
+    attack +=  choice1
+    sp -= choice1
     sceneset("black",'white', 300, '💗')
     print('You have ' + str(sp) + ' Skill Points left.')
-    charisma = int(input("Enter charisma value (Integer) "))
-    sp -= charisma
+    choice2 = int(input("Enter charisma value (Integer) "))
+    charisma += choice2
+    sp -= choice2
     sceneset("black",'white', 300, '🛡')
     print('You have ' + str(sp) + ' Skill Points left.')
-    defense = int(input("Enter defense value (Integer) "))
-    sp -= defense
+    choice3 = int(input("Enter defense value (Integer) "))
+    defense += choice3
+    sp -= choice3
     sceneset("black",'white', 300, '💰')
     print('You have ' + str(sp) + ' Skill Points left.')
-    money = int(input("Enter your money (Integer) "))
-    sp -= money
+    choice4 = int(input("Enter your money (Integer) "))
+    money += choice4
+    sp -= choice4
     print('You have ' + str(sp) + ' Skill Points left.')
   if sp < 0:
     os.system("clear")
     print("Looks like you messed up point allocation... Try again.")
+    attack = 0
+    charisma = 0
+    defense = 0
+    money = 0
     pass
   else:
     finished = 0
@@ -166,11 +185,13 @@ while True:
       if choice == "1":
         clear()
         dia("I'm really hungry, I wonder if there is food here?...")
+        sceneset('black','brown', 300, '🚪')
         nar("You find a door, do you enter? (input 1 or 2)")
         
         while True:
           choice = input("")
           if choice == "1":
+            sceneset('black','brown', 300, '👹')
             nar("You enter the door, you find a monster.")
             
             if MC.attack >= 5:
@@ -180,6 +201,7 @@ while True:
             time.sleep(15)
             break
           elif choice == "2":
+            sceneset('black','purple', 300, '🚪')
             nar("You find a wacky door, do you enter? (input 1 or 2)")
             
             while True:
@@ -189,8 +211,9 @@ while True:
                 time.sleep(15)
                 break
               elif choice == "2":
-                pass
-                #go back to other door
+                
+                time.sleep(15)
+                break
               else:
                 print("Choose 1 or 2")
                 sceneset('black','red', 300, '❌')
@@ -221,10 +244,10 @@ while True:
             
             if MC.money >= 5:
               sceneset("black",'white', 300, '🏆')
-              nar("Congradulations, you win!")
+              nar("You paid off his debt, Congradulations, you win!")
             else:
               sceneset("black",'white', 300, '🪦')
-              nar("You have been slain.")
+              nar("You were molested and sold because you didn't have enough money.")
  
             time.sleep(15)
             break
@@ -237,7 +260,7 @@ while True:
               nar("Congradulations, you win!")
             else:
               sceneset("black",'white', 300, '🪦')
-              nar("You have been slain.")
+              nar("You have been slain due to low defense.")
             time.sleep(15)
             break
           else:
@@ -252,15 +275,63 @@ while True:
     break
   elif Verified == 'N':
     clear()
-    MC = char(
-    input("Enter a name "),
-      attack = int(input("Enter attack value (Integer) ")),
-      charisma = int(input("Enter charisma value (Integer) ")),
-      defense = int(input("Enter defense value (Integer) ")),
-      money = int(input("Enter your money (Integer) "))
-      )
     
+    finished = 1
+
+    name = ""
+    attack = 0
+    charisma = 0
+    defense = 0
+    money = 0
+
+    while finished == 1:
+      name = input("Enter a name ")
+      sp = 10
+      while sp > 0:
+        choice1 = 0
+        choice2 = 0
+        choice3 = 0
+        choice4 = 0
+        sceneset("black",'white', 300, '⚔')
+        choice1 = int(input("Enter attack value (Integer) "))
+        attack +=  choice1
+        sp -= choice1
+        sceneset("black",'white', 300, '💗')
+        print('You have ' + str(sp) + ' Skill Points left.')
+        choice2 = int(input("Enter charisma value (Integer) "))
+        charisma += choice2
+        sp -= choice2
+        sceneset("black",'white', 300, '🛡')
+        print('You have ' + str(sp) + ' Skill Points left.')
+        choice3 = int(input("Enter defense value (Integer) "))
+        defense += choice3
+        sp -= choice3
+        sceneset("black",'white', 300, '💰')
+        print('You have ' + str(sp) + ' Skill Points left.')
+        choice4 = int(input("Enter your money (Integer) "))
+        money += choice4
+        sp -= choice4
+        print('You have ' + str(sp) + ' Skill Points left.')
+      if sp < 0:
+        os.system("clear")
+        print("Looks like you messed up point allocation... Try again.")
+        attack = 0
+        charisma = 0
+        defense = 0
+        money = 0
+        pass
+      else:
+        finished = 0
+        MC = char(
+          name = name,
+          attack = attack,
+          charisma = charisma,
+          defense = defense,
+          money = money
+          )
+
     print(MC)
+
 
     Verified = input('Proceed? (Y/N)')
   else:
